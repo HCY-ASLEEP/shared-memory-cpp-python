40629ef5c554617ab8fc5aa92c1596fc91c4de9d
HCY-ASLEEP <2420066864@qq.com>
2024-04-20 16:40:39 +0800

    refact: fix unclosed semaphore

--- recieve.py
+++ recieve.py
@@ -8,8 +8,16 @@ class sharedMemoryAdapter:
     def __init__(self,shared) -> None:
         self.shared=shared
         self.offset=0
+        self.data={}
+    
+    def clear(self) -> None:
+        self.offset=0
+        self.data={}
+        
+    def readData(self)->dict:
+        return self.data
 
-    def readJson(self):
+    def readJson(self)->None:
         # 读取 json 文件的长度
         jsonSizes = np.ndarray((1,), dtype=np.int32, buffer=self.shared.buf, offset=self.offset)
         self.offset+=jsonSizes.dtype.itemsize
@@ -17,48 +25,65 @@ class sharedMemoryAdapter:
         # 获取 json 的字节编码
         jsonByteData=self.shared.buf[self.offset:self.offset+jsonSize]
         self.offset+=jsonSize
-        # 将bytes数据转换为字符串
-        jsonStrData = jsonByteData.tobytes().decode('utf-8')
-        # 加载JSON数据
-        parsedJson = json.loads(jsonStrData)
-        print("Received JSON data:", parsedJson)
-    
-    def readImage(self):
+        
+        self.data['json']=jsonByteData
+
+            
+    def readImage(self)->None:
         # 获取图片的维度大小信息
         dimensions = np.ndarray((3,), dtype=np.int32, buffer=self.shared.buf, offset=self.offset)
         self.offset += 3 * dimensions.dtype.itemsize
         height, width, channels = dimensions
         # 获取图片
-        image_data = np.ndarray((height, width, channels), dtype=np.uint8, buffer=self.shared.buf, offset=self.offset)
+        mat = np.ndarray((height, width, channels), dtype=np.uint8, buffer=self.shared.buf, offset=self.offset)
         self.offset += height*width*channels 
+        
+        self.data['mat']=mat
+        
+        
+
+if __name__=='__main__':
+    shmName='shared_image_json'
+    semEmpty='cpp2python-empty'
+    # 关联到共享内存
+    semFull='cpp2python-full'
+    shared = shm.SharedMemory(name=shmName)
+    # 关联到信号量
+    empty = posix_ipc.Semaphore(semEmpty)
+    full = posix_ipc.Semaphore(semFull)
+    shma=sharedMemoryAdapter(shared)
+    while(True):
+        print('full not acquired')
+        print(empty.value)
+        print(full.value)
+
+        shma.clear()
+
+        full.acquire()
+        
+        shma.readJson()
+        shma.readImage()
+        
+        empty.release()
+        
+        data=shma.readData()
+        
+        jsonByteData=data['json']
+        # 将bytes数据转换为字符串
+        jsonStrData = jsonByteData.tobytes().decode('utf-8')
+        # 加载JSON数据
+        parsedJson = json.loads(jsonStrData)
+        print('Received JSON data:', parsedJson)
+        
+        mat=data['mat']
         # 展示图片
-        cv2.imshow("Image from C++", image_data)
+        cv2.imshow('Image from C++', mat)
         key=cv2.waitKey(0)
         # 检查用户是否按下了 'q' 键，如果是则退出程序
         if key == ord('q'):
             cv2.destroyAllWindows()  # 关闭所有OpenCV窗口
 
 
-if __name__=="__main__":
-    shmName="shared_image_json"
-    semEmpty="cpp2python-empty"
-    semFull="cpp2python-full"
-    # 关联到共享内存
-    shared = shm.SharedMemory(name=shmName)
-    # 关联到信号量
-    empty = posix_ipc.Semaphore(semEmpty)
-    full = posix_ipc.Semaphore(semFull)
-    while(True):
-        print("full not acquired")
-        
-        full.acquire()
-        
-        print("full acquired")
-        
-        shma=sharedMemoryAdapter(shared)
-        shma.readJson()
-        shma.readImage()
-        
-        empty.release()
+
     
     
--- send.cpp
+++ send.cpp
@@ -1,94 +1,117 @@
-#include <opencv2/opencv.hpp>
-#include <boost/interprocess/shared_memory_object.hpp>
 #include <boost/interprocess/mapped_region.hpp>
-#include <semaphore.h>
+#include <boost/interprocess/shared_memory_object.hpp>
 #include <fcntl.h>
-#include <sys/stat.h>
+#include <opencv2/opencv.hpp>
 #include <rapidjson/document.h>
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/writer.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+
+class rapidjsonAdapter {
+  private:
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer;
+
+  public:
+    std::string doc2Str(rapidjson::Document &doc) {
+        buffer.Clear();
+        writer.Reset(buffer);
+        doc.Accept(writer);
+        return buffer.GetString();
+    }
+};
 
 class shareMemoryAdapter {
   private:
     boost::interprocess::shared_memory_object shm;
     boost::interprocess::mapped_region region;
-    char* shmp; // share memory pointer
-    sem_t* empty;
-    sem_t* full;
+    const char *shmName;
+    const char *semEmptyName;
+    const char *semFullName;
+    char *shmp; // share memory pointer
+    sem_t *empty;
+    sem_t *full;
     int offset;
-    void write(cv::Mat& mat) {
-        int size= mat.total() * mat.elemSize();
+    rapidjsonAdapter rjAdapter;
+    void write(cv::Mat &mat) {
+        int size = mat.total() * mat.elemSize();
         // Write image dimensions to shared memory
-        int* dimensions = (int*)(shmp + offset);
-        dimensions[0] = mat.rows;     // Height
-        dimensions[1] = mat.cols;     // Width
+        int *dimensions = (int *)(shmp + offset);
+        dimensions[0] = mat.rows;       // Height
+        dimensions[1] = mat.cols;       // Width
         dimensions[2] = mat.channels(); // Channels
-        offset+=sizeof(int)*3;
+        offset += sizeof(int) * 3;
         // Copy image data to shared memory
         std::memcpy(shmp + offset, mat.data, size);
-        offset+=size;
+        offset += size;
     }
-    void write(rapidjson::Document& doc) {
+    void write(std::string &jsonStr) {
         // Convert JSON document to string
-        rapidjson::StringBuffer buffer;
-        rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-        doc.Accept(writer);
-        std::string data = buffer.GetString();
-        int size=data.size();
+        int size = jsonStr.size();
         // Write size
-        int* head=(int*)(shmp+offset);
-        head[0]=size;
-        offset+=sizeof(int);
+        int *head = (int *)(shmp + offset);
+        head[0] = size;
+        offset += sizeof(int);
         // Write JSON data to shared memory
-        std::memcpy(shmp+offset, data.c_str(), size);
-        offset+=size;
+        std::memcpy(shmp + offset, jsonStr.c_str(), size);
+        offset += size;
     }
 
   public:
-    shareMemoryAdapter(const char* shmName,const int size,const char* semEmptyName,const char* semFullName) {
+    shareMemoryAdapter(const char *shmName, const int size,
+                       const char *semEmptyName, const char *semFullName)
+        : shmName(shmName), semEmptyName(semEmptyName),
+          semFullName(semFullName) {
         // Set access flags
-        auto flags=boost::interprocess::open_or_create;
+        auto flags = boost::interprocess::open_or_create;
         // Set access mode
-        auto mode=boost::interprocess::read_write;
+        auto mode = boost::interprocess::read_write;
         // Create/Open shared memory
-        shm=std::move(boost::interprocess::shared_memory_object(flags, shmName, mode));
+        shm = std::move(
+            boost::interprocess::shared_memory_object(flags, shmName, mode));
         // Adjust size as needed
         shm.truncate(size);
         // Map the whole shared memory in this process
-        region=std::move(boost::interprocess::mapped_region(shm,mode));
+        region = std::move(boost::interprocess::mapped_region(shm, mode));
         // Get share memory pointer
-        shmp=static_cast<char*>(region.get_address());
+        shmp = static_cast<char *>(region.get_address());
         // Create semaphore
         // 0664 : access ,means rw-rw-r--
-        empty = sem_open(semEmptyName,O_CREAT,0664,1);
-        full = sem_open(semFullName,O_CREAT,0664,0);
+        sem_unlink(semEmptyName);
+        sem_unlink(semFullName);
+        empty = sem_open(semEmptyName, O_CREAT, 0664, 1);
+        full = sem_open(semFullName, O_CREAT, 0664, 0);
         // occupied offset
-        offset=0;
+        offset = 0;
     }
-    void clear() {
-        offset=0;
+    ~shareMemoryAdapter() {
+        boost::interprocess::shared_memory_object::remove(shmName);
+        sem_unlink(semEmptyName);
+        sem_unlink(semFullName);
     }
-    void write(rapidjson::Document& jsonDoc, cv::Mat& mat) {
+    void clear() { offset = 0; }
+    void write(rapidjson::Document &jsonDoc, cv::Mat &mat) {
+        std::string &&jsonStr = rjAdapter.doc2Str(jsonDoc);
         // Waiting for empty buffer
         sem_wait(empty);
         // Write into shm
-        write(jsonDoc);
+        write(jsonStr);
         write(mat);
         // Signal full buffer
         sem_post(full);
     }
 };
 
-
 int main() {
-    const int shmBufSize=2000000;
-    const char* shmName="shared_image_json";
-    const char* semEmptyName="cpp2python-empty";
-    const char* semFullName="cpp2python-full";
-    const char* imagePath="/home/devenv/2024-01-19_13-35.png";
+    const int shmBufSize = 2000000;
+    const char *shmName = "shared_image_json";
+    const char *semEmptyName = "cpp2python-empty";
+    const char *semFullName = "cpp2python-full";
+    const char *imagePath = "/home/devenv/2024-03-24_18-39.png";
     // Make sure you have set the correct size of the shared buffer
-    shareMemoryAdapter shm(shmName,shmBufSize,semEmptyName,semFullName);
-    while(true) {
+    shareMemoryAdapter shm(shmName, shmBufSize, semEmptyName, semFullName);
+    while (true) {
         // OpenCV operations
         cv::Mat image = cv::imread(imagePath);
         // Create RapidJSON document
@@ -100,7 +123,6 @@ int main() {
         doc.AddMember(key, value, doc.GetAllocator());
         // Everytime to write sth, make sure that you have clear the history
         shm.clear();
-        shm.write(doc,image);
+        shm.write(doc, image);
     }
 }
-
